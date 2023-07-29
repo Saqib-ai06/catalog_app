4c4981be8bd4bdbb5ae43630e1fcffa142a18a03
Saqib-ai06 <132567462+Saqib-ai06@users.noreply.github.com>
2023-07-29 16:13:15 +0530

    Made a login page and learned about routs

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
       darkTheme: ThemeData(
         brightness: Brightness.dark,
       ),
+      initialRoute: "/home",
       routes: {
         "/": (context) => LoginPage(),
         "/home": (context) => HomePage(),
